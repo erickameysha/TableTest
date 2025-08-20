4fefac6cecba346cfc925dd01b3fa5d71db4838f
ekameyshaaa <ekameyshaaa@gmail.com>
2025-08-21 02:02:22 +0300

    init draft

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Tablee from "./components/Table.tsx";
+
+function App() {
+
+    return (
+        <div>
+            <Tablee/>
+        </div>
+    )
+}
+
+export default App
